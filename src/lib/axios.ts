71386d2f363ff4428a012ea91ff072732122830f
VISHAL DUBEY <DUBEYVISHAL917@GMAIL.COM>
2025-02-16 15:11:54 +0530

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 // Use the API URL from environment variables or fallback to localhost
-const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:3000/api";
+const API_BASE_URL ='https://alfredtask-akvx.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
