5336f6671e2c1d01a316c473306dc6e3a0fa33b4
VISHAL DUBEY <DUBEYVISHAL917@GMAIL.COM>
2025-02-17 03:02:35 +0530

    Update Flashcard.js

--- server/src/models/Flashcard.js
+++ server/src/models/Flashcard.js
@@ -23,7 +23,7 @@ const flashcardSchema = new mongoose.Schema(
     },
     nextReviewDate: {
       type: Date,
-      default: Date.now
+      default: () => new Date(Date.now() + 5.5 * 60 * 60 * 1000) // Store as IST
     }
   },
   { timestamps: true }
@@ -36,38 +36,38 @@ flashcardSchema.methods.calculateNextReviewDate = function () {
 
   switch (this.box) {
     case 1:
-      daysToAdd = 0; // Review today
+      daysToAdd = 0;
       break;
     case 2:
-      daysToAdd = 1; // Review in 1 day
+      daysToAdd = 1;
       break;
     case 3:
-      daysToAdd = 3; // Review in 3 days
+      daysToAdd = 3;
       break;
     case 4:
-      daysToAdd = 7; // Review in 7 days
+      daysToAdd = 7;
       break;
     case 5:
-      daysToAdd = 15; // Review in 15 days
+      daysToAdd = 15;
       break;
     case 6:
-      daysToAdd = 30; // Review in 30 days
+      daysToAdd = 30;
       break;
     default:
       daysToAdd = 0;
   }
 
-  return new Date(now.getTime() + daysToAdd * 24 * 60 * 60 * 1000);
+  const nextDateUTC = new Date(now.getTime() + daysToAdd * 24 * 60 * 60 * 1000);
+  return new Date(nextDateUTC.getTime() + 5.5 * 60 * 60 * 1000); // Convert to IST
 };
 
 // Function to update the flashcard based on user's answer
 flashcardSchema.methods.processReview = function (correct) {
   if (correct) {
-    this.box = Math.min(this.box + 1, 6); // Move to next box (max box 6)
+    this.box = Math.min(this.box + 1, 6);
   } else {
-    this.box = 1; // Reset to Box 1 if answered incorrectly
+    this.box = 1;
   }
-
   this.nextReviewDate = this.calculateNextReviewDate();
 };
 
